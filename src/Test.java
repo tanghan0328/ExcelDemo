96891e4c4b63de7b8902fd095fcf243ffae36d7d
tangwei <530959820@qq.com>
2017-07-25 10:08:56 +0800

    提交工具的修改
    提交工具的修改

---
+++ src/Test.java
@@ -0,0 +1,126 @@
+
+import java.io.File;
+
+import java.io.FileInputStream;
+
+import java.io.InputStream;
+
+
+
+import jxl.Cell;
+
+import jxl.CellType;
+
+import jxl.Sheet;
+
+import jxl.Workbook;
+
+import jxl.write.Label;
+
+
+
+public class Test
+
+{
+
+    public static void main(String[] args)
+
+    {
+
+        jxl.Workbook readwb = null;
+
+        try
+
+        {
+
+            //构建Workbook对象, 只读Workbook对象
+
+            //直接从本地文件创建Workbook
+            System.out.println("======"+File.separator);
+            InputStream instream = new FileInputStream("/Users/tangtang/Desktop/1.xlsx");
+
+            readwb = Workbook.getWorkbook(instream);
+
+
+
+            //Sheet的下标是从0开始
+
+            //获取第一张Sheet表
+
+            Sheet readsheet = readwb.getSheet(0);
+
+            //获取Sheet表中所包含的总列数
+
+            int rsColumns = readsheet.getColumns();
+
+            //获取Sheet表中所包含的总行数
+
+            int rsRows = readsheet.getRows();
+
+            //获取指定单元格的对象引用
+
+            for (int i = 2; i < rsRows; i++)
+
+            {
+
+                for (int j = 2; j < rsColumns; j++)
+
+                {
+
+                    Cell cell = readsheet.getCell(j, i);
+
+                    System.out.print(cell.getContents() + " ");
+
+                }
+
+                System.out.println();
+
+            }
+
+
+
+            //利用已经创建的Excel工作薄,创建新的可写入的Excel工作薄
+
+            jxl.write.WritableWorkbook wwb = Workbook.createWorkbook(new File(
+
+                    File.separator +"/Users/tangtang/Desktop/Bixin-APP-Localization1.xlsx"), readwb);
+
+            //读取第一张工作表
+
+            jxl.write.WritableSheet ws = wwb.getSheet(0);
+
+            //获得第一个单元格对象
+
+            jxl.write.WritableCell wc = ws.getWritableCell(0, 0);
+
+            //判断单元格的类型, 做出相应的转化
+
+            if (wc.getType() == CellType.LABEL)
+
+            {
+
+                Label l = (Label) wc;
+
+                l.setString("新姓名");
+
+            }
+
+            //写入Excel对象
+
+            wwb.write();
+
+            wwb.close();
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+        } finally {
+
+            readwb.close();
+
+        }
+
+    }
+
+}
---
+++ src/Test01.java
@@ -0,0 +1,112 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.*;
+
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
+import org.apache.poi.hssf.usermodel.HSSFFont;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.util.CellRangeAddress;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+
+import java.io.FileOutputStream;
+
+import java.io.*;
+
+
+public class Test01 {
+
+
+    public static void main(String[] args) throws Exception {
+        ArrayList<String> strArray = new ArrayList();
+        ArrayList<String> enArray = new ArrayList();
+        ArrayList<String> jaArray = new ArrayList();
+        ArrayList<String> spArray = new ArrayList();
+        int number = 0;
+        //创建输入流
+        FileInputStream fis = new FileInputStream(new File("/Users/tangtang/Desktop/1.xlsx"));
+        //通过构造函数传参
+        XSSFWorkbook workbook = new XSSFWorkbook(fis);
+        //获取工作表
+        XSSFSheet sheet = workbook.getSheetAt(0);
+        //获取行,行号作为参数传递给getRow方法,第一行从0开始计算
+        XSSFRow row = sheet.getRow(3);
+        //总行数
+        int trLength = sheet.getLastRowNum();
+        //总列数
+        int tdLength = row.getLastCellNum();
+        for(int index =2;index<trLength;index++){
+            for(int indexY=1;indexY<5;indexY++){
+
+                XSSFRow row1 = sheet.getRow(index);
+                //得到Excel工作表指定行的单元格
+                XSSFCell cell = row1.getCell(indexY);
+                if(cell==null||cell.equals("")||cell.getCellType() ==XSSFCell.CELL_TYPE_BLANK){
+                    break;
+                }
+                    String cellValue = cell.getStringCellValue();
+                System.out.println("值是多少===》"+cellValue);
+                if(indexY == 1){
+                    strArray.add(cellValue);
+                }
+                if(indexY == 2){
+                    enArray.add(cellValue);
+                }
+                if(indexY == 3){
+                    jaArray.add(cellValue);
+                }
+                if(indexY == 4){
+                    spArray.add(cellValue);
+                }
+            }
+            number++;
+        }
+        fis.close();
+
+        FileOutputStream out = null;
+
+        FileOutputStream outSTr = null;
+
+        BufferedOutputStream Buff=null;
+
+        FileWriter fw = null;
+
+        int count=1000;//写文件行数
+        try{
+            out = new FileOutputStream(new File("/Users/tangtang/Desktop/zn.text"));
+            System.out.println("======="+number);
+            for (int i = 0; i < enArray.size(); i++) {
+                 out.write(("\""+strArray.get(i) + "\" = \"" + enArray.get(i)+"\";\r\n").getBytes());
+             }
+
+             out = new FileOutputStream(new File("/Users/tangtang/Desktop/ja.text"));
+
+            System.out.println("======="+number);
+            for (int i = 0; i < jaArray.size(); i++) {
+                out.write(("\""+strArray.get(i) + "\" = \"" + jaArray.get(i)+"\";\r\n").getBytes());
+             }
+
+             out = new FileOutputStream(new File("/Users/tangtang/Desktop/sp.text"));
+
+             System.out.println("======="+number);
+             for (int i = 0; i < spArray.size(); i++) {
+                out.write(("\""+strArray.get(i) + "\" = \"" + spArray.get(i)+"\";\r\n").getBytes());
+             }
+            out.close();
+        }catch (Exception e){
+
+            e.printStackTrace();
+        }
+
+    }
+
+
+}
